c564bf5138482e148c813f12d40a960d55747a44
rizapranata <riza.ari@wgs-id.com>
2023-12-01 21:37:20 +0700

    implement redux toolkit

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,19 +1,24 @@
 import { StyleSheet, View, Text } from "react-native";
 import MealsList from "../components/MealsList/MealsList";
-import { useContext } from "react";
-import { FavoritesContext } from "../store/context/favorites-context";
+// import { useContext } from "react";
+// import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/dummy-data";
+import { useSelector } from "react-redux";
 
 function FavoritesScreen() {
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  // const favoriteMealsCtx = useContext(FavoritesContext);
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
+
   const favoriteMeals = MEALS.filter((meal) =>
-    favoriteMealsCtx.ids.includes(meal.id)
+    favoriteMealIds.includes(meal.id)
   );
 
   if (favoriteMeals.length === 0) {
-    return <View style={styles.rootContainer}>
-      <Text style={styles.text}>You have no favorite meals yet.</Text>
-    </View>
+    return (
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>You have no favorite meals yet.</Text>
+      </View>
+    );
   }
 
   return <MealsList items={favoriteMeals} />;
@@ -25,11 +30,11 @@ const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
     justifyContent: "center",
-    alignItems: 'center'
+    alignItems: "center",
   },
   text: {
     fontSize: 18,
-    fontWeight: 'bold',
-    color: 'white'
-  }
-})
+    fontWeight: "bold",
+    color: "white",
+  },
+});
